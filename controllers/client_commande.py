68ea8aafc48fb087175987ec198e627497da099d
Damien Lugbull <damienlugbull@gmail.com>
2026-03-13 13:13:10 +0100

    Implement new database to show commands for client

--- controllers/client_commande.py
+++ controllers/client_commande.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from connexion_db import get_db
 
 client_commande = Blueprint('client_commande', __name__,
-                        template_folder='templates')
+                            template_folder='templates')
 
 
 # validation de la commande : partie 2 -- vue pour choisir les adresses (livraision et facturation)
@@ -15,17 +15,17 @@ def client_commande_valide():
     mycursor = get_db().cursor()
     id_client = session['id_user']
     sql = '''SELECT id_gant, nom_gant AS nom, prix_gant AS prix, quantite
-    FROM gant
-    JOIN ligne_panier ON gant.id_gant = ligne_panier.gant_id
-    WHERE ligne_panier.utilisateur_id=%s;
-    '''
+             FROM gant
+                      JOIN ligne_panier ON gant.id_gant = ligne_panier.gant_id
+             WHERE ligne_panier.utilisateur_id=%s; \
+          '''
     mycursor.execute(sql, (id_client,))
     gants_panier = mycursor.fetchall()
     if len(gants_panier) >= 1:
         sql = '''SELECT SUM(prix_gant*quantite) AS prix_total
-        FROM gant
-        JOIN ligne_panier on gant.id_gant=ligne_panier.gant_id
-        WHERE ligne_panier.utilisateur_id=%s'''
+                 FROM gant
+                          JOIN ligne_panier on gant.id_gant=ligne_panier.gant_id
+                 WHERE ligne_panier.utilisateur_id=%s'''
         mycursor.execute(sql, (id_client,))
         prix_total = mycursor.fetchone()["prix_total"]
     else:
@@ -49,20 +49,20 @@ def client_commande_add():
 
     id_client = session['id_user']
     sql = '''SELECT *, quantite*prix_gant AS prix
-    FROM ligne_panier
-    JOIN gant ON ligne_panier.gant_id=gant.id_gant
-    WHERE utilisateur_id=%s'''
+             FROM ligne_panier
+                      JOIN gant ON ligne_panier.gant_id=gant.id_gant
+             WHERE utilisateur_id=%s'''
     mycursor.execute(sql, (id_client,))
     items_ligne_panier = mycursor.fetchall()
     if items_ligne_panier is None or len(items_ligne_panier) < 1:
         flash(u'Pas de gants dans le ligne_panier', 'alert-warning')
         mycursor.close()
         return redirect('/client/gant/show')
-                                           # https://pynative.com/python-mysql-transaction-management-using-commit-rollback/
+        # https://pynative.com/python-mysql-transaction-management-using-commit-rollback/
     a = datetime.now()
 
     sql = '''INSERT INTO commande (date_achat, etat_id, utilisateur_id)
-    VALUES (%s, 1, %s)'''
+             VALUES (%s, 1, %s)'''
     mycursor.execute(sql, (a,id_client))
     get_db().commit()
 
@@ -73,12 +73,12 @@ def client_commande_add():
     for item in items_ligne_panier:
         print(item)
         sql = '''DELETE FROM ligne_panier
-        WHERE ligne_panier.gant_id=%s AND ligne_panier.utilisateur_id = %s'''
+                 WHERE ligne_panier.gant_id=%s AND ligne_panier.utilisateur_id = %s'''
         mycursor.execute(sql, (item["gant_id"], item["utilisateur_id"]))
         get_db().commit()
         sql = '''INSERT INTO ligne_commande (commande_id, gant_id, quantite, prix)
-        VALUES (%s, %s, %s, %s)
-        '''
+                 VALUES (%s, %s, %s, %s) \
+              '''
         mycursor.execute(sql, (id_commande, item["gant_id"], item["quantite"], item["prix"]))
         get_db().commit()
     flash(u'Commande ajoutée','alert-success')
@@ -93,20 +93,20 @@ def client_commande_show():
     mycursor = get_db().cursor()
     id_client = session['id_user']
     sql = '''
-     SELECT commande.etat_id,
-    utilisateur.login,
-    SUM(ligne_commande.quantite) as nbr_gants,
-    SUM(ligne_commande.prix * ligne_commande.quantite) as prix_total,
-    commande.date_achat,
-    etat.libelle_etat as libelle,
-    commande.id_commande
-     FROM commande
-     JOIN utilisateur on utilisateur.id_utilisateur = commande.utilisateur_id
-     JOIN ligne_commande on ligne_commande.commande_id = commande.id_commande
-     JOIN etat on etat.id_etat = commande.etat_id
-     WHERE commande.utilisateur_id=%s
-     GROUP BY commande.id_commande
-     ORDER BY etat_id, date_achat DESC;'''
+          SELECT commande.etat_id,
+                 utilisateur.login,
+                 SUM(ligne_commande.quantite) as nbr_gants,
+                 SUM(ligne_commande.prix * ligne_commande.quantite) as prix_total,
+                 commande.date_achat,
+                 etat.libelle_etat as libelle,
+                 commande.id_commande
+          FROM commande
+                   JOIN utilisateur on utilisateur.id_utilisateur = commande.utilisateur_id
+                   JOIN ligne_commande on ligne_commande.commande_id = commande.id_commande
+                   JOIN etat on etat.id_etat = commande.etat_id
+          WHERE commande.utilisateur_id=%s
+          GROUP BY commande.id_commande
+          ORDER BY etat_id, date_achat DESC;'''
     mycursor.execute(sql, (id_client,))
     commandes = mycursor.fetchall()
     gants_commande = None
@@ -114,15 +114,16 @@ def client_commande_show():
     id_commande = request.args.get('id_commande', None)
     if id_commande != None:
         sql = '''SELECT gant.nom_gant as nom,
-        ligne_commande.quantite,
-        ligne_commande.prix,
-        (ligne_commande.quantite * ligne_commande.prix) as prix_ligne,
-        commande.etat_id,
-        commande.id_commande AS id
-        FROM ligne_commande
-        JOIN gant ON ligne_commande.gant_id = gant.id_gant
-        JOIN commande on commande.id_commande = ligne_commande.commande_id
-        WHERE ligne_commande.commande_id = %s AND commande.utilisateur_id=%s;'''
+                        ligne_commande.quantite,
+                        ligne_commande.prix,
+                        (ligne_commande.quantite * ligne_commande.prix) as prix_ligne,
+                        commande.etat_id,
+                        commande.id_commande AS id
+                 FROM ligne_commande
+                          JOIN declinaison_gant ON ligne_commande.declinaison_gant_id = declinaison_gant.id_declinaison_gant
+                          JOIN gant ON declinaison_gant.gant_id = gant.id_gant
+                          JOIN commande on commande.id_commande = ligne_commande.commande_id
+                 WHERE ligne_commande.commande_id = %s AND commande.utilisateur_id=%s;'''
         mycursor.execute(sql, (id_commande,id_client))
         gants_commande = mycursor.fetchall()
 
@@ -130,7 +131,22 @@ def client_commande_show():
             flash("Cette commande ne vous appartient pas", "alert-warning")
 
         # partie 2 : selection de l'adresse de livraison et de facturation de la commande selectionnée
-        sql = ''' selection des adressses '''
+        sql = '''SELECT a_fact.nom_adresse AS nom_facturation,
+                        a_fact.rue AS rue_facturation,
+                        a_fact.code_postal AS code_postal_facturation,
+                        a_fact.ville AS ville_facturation,
+                        a_livre.nom_adresse AS nom_livraison,
+                        a_livre.rue AS rue_livraison,
+                        a_livre.code_postal AS code_postal_livraison,
+                        a_livre.ville AS ville_livraison,
+                        if(a_livre.id_adresse = a_fact.id_adresse, "adresse_identique", null) AS adresse_identique
+                 FROM commande
+                          JOIN adresse a_fact ON a_fact.id_adresse = commande.adresse_id_fact
+                          JOIN adresse a_livre ON a_livre.id_adresse = commande.adresse_id_livre
+                 WHERE commande.utilisateur_id=%s AND commande.id_commande=%s;'''
+        mycursor.execute(sql, (id_client, id_commande))
+        commande_adresses = mycursor.fetchone()
+        print(commande_adresses)
     mycursor.close()
     return render_template('client/commandes/show.html'
                            , commandes=commandes
